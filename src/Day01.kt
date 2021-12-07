c8365fc8ef2f346931fe25d46cf3932ca296171c
Thierry <titimoby@gmail.com>
2021-12-07 09:18:31 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input
+                // prendre les entiers 2 par 2
+                .windowed(2)
+                // compter les cas ou le second est supérieur au premier
+                .count { (first, second) -> second > first }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input
+                // prendre les entiers 3 par 3
+                .windowed(3)
+                // regarder 2 groupes par 2 groupes d'entiers
+                .windowed(2)
+                // compter les cas ou la somme des entiers du second groupe est supérieure
+                .count { (first, second) -> second.sum() > first.sum() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // comparer A + B + C et B + C + D
+    // revient  à comparer A et D
+    fun part2Alternative(input: List<Int>): Int {
+        return input
+                // prendre les entiers 4 par 4
+                .windowed(4)
+                // compter les cas où le quatrième entier est supérieur au premier
+                .count { it[3] > it[0] }
+    }
 
-    val input = readInput("Day01")
+    // test avec les données de l'exemple de l'énoncé
+    val testInput = readInputAsInt("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
+    check(part2Alternative(testInput) == 5)
+
+    val input = readInputAsInt("Day01")
     println(part1(input))
     println(part2(input))
+    println(part2Alternative(input))
 }
